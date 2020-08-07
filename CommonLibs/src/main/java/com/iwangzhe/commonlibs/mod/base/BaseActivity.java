90ec28efa632ebd9baf8249f7e24b32ecbc03849
Madwheel <374211103@qq.com>
2020-08-07 14:54:00 +0800

    aaa

--- CommonLibs/src/main/java/com/iwangzhe/commonlibs/mod/base/BaseActivity.java
+++ CommonLibs/src/main/java/com/iwangzhe/commonlibs/mod/base/BaseActivity.java
@@ -5,13 +5,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
 
 /**
  * author : 亚辉
